1a472666788f93f9c95e69143aa24d2cb968ba4a
Sarthak Roy <sarthakroy2002@gmail.com>
2026-04-25 20:14:48 +0530

    feat: Initial Codebase
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

---
+++ src/main/java/com/sarthak/inventorymgmt/InventorymgmtApplication.java
@@ -0,0 +1,13 @@
+package com.sarthak.inventorymgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorymgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorymgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarthak/inventorymgmt/InventorymgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarthak.inventorymgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorymgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
