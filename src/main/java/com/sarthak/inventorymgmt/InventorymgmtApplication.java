c15772a8a8db1d70c78dfb846d74c4bb9baa2b79
Sarthak Roy <sarthakroy2002@gmail.com>
2026-04-26 12:34:22 +0530

    feat: Add default server port
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- src/main/java/com/sarthak/inventorymgmt/InventorymgmtApplication.java
+++ src/main/java/com/sarthak/inventorymgmt/InventorymgmtApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @EnableTransactionManagement
+@Slf4j
 public class InventorymgmtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventorymgmtApplication.class, args);
+		log.info("Inventory Management Application Started @" + System.getProperty("server.port"));
 	}
 
 }
